b6777ff49c71d2390b6c084a07e3f7fd74f8e358
dev-jaemin <jay.kimjaemin@gmail.com>
2022-06-30 10:42:26 +0900

    permission받는 코드

--- app/src/main/java/com/example/firstflow/MainActivity.java
+++ app/src/main/java/com/example/firstflow/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.firstflow;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,86 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            checkVerify(Manifest.permission.READ_CONTACTS);
+        }
+        else
+        {
+            startApp();
+        }
     }
+    public void startApp()
+    {
+
+    }
+
+
+    /*
+    * =================== 권한 요청 코드 ==================
+    *
+    * */
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public void checkVerify(String permissionName)
+    {
+        if (
+                checkSelfPermission(permissionName) != PackageManager.PERMISSION_GRANTED )
+        {
+            // Should we show an explanation?
+            if (shouldShowRequestPermissionRationale(permissionName))
+            {
+                // ...
+            }
+            requestPermissions(new String[]{ permissionName },
+                    1);
+        }
+        else
+        {
+            startApp();
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1)
+        {
+            if (grantResults.length > 0)
+            {
+                for (int i=0; i<grantResults.length; ++i)
+                {
+                    if (grantResults[i] == PackageManager.PERMISSION_DENIED)
+                    {
+                        // 하나라도 거부한다면.
+                        new AlertDialog.Builder(this).setTitle("알림").setMessage("권한을 허용해주셔야 앱을 이용할 수 있습니다.")
+                                .setPositiveButton("종료", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        MainActivity.this.finish();
+                                    }
+                                }).setNegativeButton("권한 설정", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                                .setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                                        getApplicationContext().startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                                    }
+                                }).setCancelable(false).show();
+
+                        return;
+                    }
+                }
+                startApp();
+            }
+        }
+    }
+
+
+
 }
+
